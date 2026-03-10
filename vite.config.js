f5cb7ad87a1c81feb4304b687e916c1305485b97
me <lorraobare@gmail.com>
2026-03-10 20:17:19 +0300

    set base to allow render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import process from 'process'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/lorra-portfolio-/', 
+  base: process.env.GITHUB_ACTIONS ? '/lorra-portfolio-/' : '/',
 })
